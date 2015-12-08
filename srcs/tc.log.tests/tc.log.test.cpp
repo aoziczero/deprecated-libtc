6abd6d5c42d40a254269cb83676237b177c8b40c
suhyang i <suhyangi@suhyang-ui-MacBook-Pro.local>
2015-12-08 18:16:53 +0900

    tc.log start

---
+++ srcs/tc.log.tests/tc.log.test.cpp
@@ -0,0 +1,17 @@
+// tc.log.test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#if defined( TC_WIN32 )
+#pragma comment( lib ,"gtest")
+#pragma comment( lib ,"tc.log.lib")
+#endif
+
+
+int main( int argc , char* argv[] )
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
